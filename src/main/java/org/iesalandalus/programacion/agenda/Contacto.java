e6cadd037fd80083c91553ff6a7a962383bf9ff6
Carlos <Carlos@192.168.10.102>
2018-12-04 18:18:10 +0100

    3. Crea la clase Contacto con sus atributos correspondientes

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.agenda;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Contacto {
+    private String nombre;
+    private String telefono;
+    private String correo;
+}
