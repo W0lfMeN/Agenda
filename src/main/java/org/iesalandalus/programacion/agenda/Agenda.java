430cf103608dd166050feab99a3a440f4bba45a3
Carlos <Carlos@192.168.10.102>
2018-12-10 01:08:25 +0100

    8. Crea el constructor y los metodos get que se especifican. el metodo getContactos devolverá una copia del array de contactos

--- src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -11,8 +11,26 @@ package org.iesalandalus.programacion.agenda;
  * @author Carlos
  */
 //Crea la clase Agenda con sus atributos
-public class Agenda {
+public class Agenda {    
     private static final int MAX_CONTACTOS=100;
     private int numContactos;
+    private Contacto [] contacto=null;
     
+    
+    public Agenda (int numContactos){
+        if (numContactos>0){
+            this.numContactos=numContactos;
+        }else{
+            throw new IllegalArgumentException("Se ha introducido un numero no válido");
+        }
+    }
+
+    public int getNumContactos() {
+        return numContactos;
+    }
+
+    public Contacto [] getContacto() {
+        return contacto;
+    }
 }
+
