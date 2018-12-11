1b9eb20585a52e6330a0e01b3fc2b0cd694ac917
Carlos <Carlos@192.168.10.102>
2018-12-12 00:48:39 +0100

    Crea el método anadir para añadir un contacto a la agenda de forma que ésta se quede ordenada por orden de inserción y sin que admita contactos repetidos. Apóyate en los métodos privados buscarPrimerIndiceComprobandoExistencia e IndiceNoSuperaTamano. El método debe informar de todos los posibles errores mediante la excepcion OperationNotSupportedException: ya existe ese contacto, el array está lleno, etc.

--- src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package org.iesalandalus.programacion.agenda;
+import javax.naming.OperationNotSupportedException;
 
 
 /**
@@ -12,11 +13,11 @@ package org.iesalandalus.programacion.agenda;
  */
 //Crea la clase Agenda con sus atributos
 public class Agenda {    
-    private static final int MAX_CONTACTOS=100;
-    private int numContactos;
-    private Contacto [] contacto=null;
+    private static final int MAX_CONTACTOS=5;
+    private int numContactos=0;
+    private Contacto [] contactos=null;
     
-    
+    //crea el constructor y los metodos metodos get que se especifican
     public Agenda (int numContactos){
         if (numContactos>0){
             this.numContactos=numContactos;
@@ -24,13 +25,51 @@ public class Agenda {
             throw new IllegalArgumentException("Se ha introducido un numero no válido");
         }
     }
-
+    
+    public void aniadir (Contacto contacto) throws OperationNotSupportedException{ //lo cambio a aniadir para el test
+        int indice=0;
+        try{
+            indice=buscarPrimerIndiceComprobadoExistencia(contacto);
+        }catch(OperationNotSupportedException e){
+            e.getMessage();
+            }
+        if (indiceNoSuperaTamano(indice)){
+            contactos[indice]=contacto;
+        }else{
+            throw new OperationNotSupportedException("El array esta lleno");
+        }
+    }
+    
+    private int buscarPrimerIndiceComprobadoExistencia (Contacto contacto)throws OperationNotSupportedException {
+        int indice=0;
+        boolean encontrado=false;
+        for(int i=0;i<contactos.length && !encontrado;i++){
+            if(contactos[i]==null){
+                encontrado=true;
+                indice=i;
+            }else{
+                if(contactos[i].equals(contacto)){
+                    throw new OperationNotSupportedException("Ya existe ese contacto");
+                }
+            }
+        }
+        return indice;
+    }
+        
+    private boolean indiceNoSuperaTamano(int i){
+        if(i>=contactos.length){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
     public int getNumContactos() {
         return numContactos;
     }
 
     public Contacto [] getContacto() {
-        return contacto;
+        return contactos;
     }
 }
 
